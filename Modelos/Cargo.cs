88413b7239af00361d6537c2b57925aee45fa932
S0ntyrr <100627317+S0ntyrr@users.noreply.github.com>
2025-10-02 08:34:06 -0500

    Cambio Cargo

--- Modelos/Cargo.cs
+++ Modelos/Cargo.cs
@@ -1,14 +1,8 @@
 namespace ERPEmpleados.Models;
 
-public class Empleado
+public class Cargo
 {
     public int Id { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Documento { get; set; }
-    public DateTime FechaContratacion { get; set; }
-    public decimal Salario { get; set; }
-
-    public int CargoId { get; set; }
-    public Cargo Cargo { get; set; }
+    public string NombreCargo { get; set; }
+    public string Departamento { get; set; }
 }
